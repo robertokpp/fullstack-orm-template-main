63d1c04b492bfb9869bf89722bcbd8f2550ad5a6
Roberto Kennedy <roberto.kpp@gmail.com>
2026-02-20 21:29:46 -0500

    feat: implementar CRUD para perguntas e adicionar migração de chave estrangeira

--- src/controllers/questions-controller.ts
+++ src/controllers/questions-controller.ts
@@ -1,22 +1,51 @@
 import { Request, Response } from "express";
 import { prisma } from "@/prisma";
+import { Console } from "console";
 
 class QuestionsController {
   async index(request: Request, response: Response) {
-    return response.json();
+    const questions = await prisma.question.findMany({
+      where: {
+        tittle: {
+          contains: request.query.tittle?.toString().trim(),
+          mode: "insensitive",
+        },
+      },
+      orderBy: {
+        tittle: "asc",
+      },
+    });
+
+    return response.json(questions);
   }
 
   async create(request: Request, response: Response) {
     const { tittle, content, userId } = request.body;
-    await prisma.question.create({data: {tittle,content, userId}, });
+    await prisma.question.create({ data: { tittle, content, userId } });
     return response.status(201).json();
   }
 
   async update(request: Request, response: Response) {
+    const { id } = request.params;
+    const { tittle, content } = request.body;
+
+    await prisma.question.update({
+      data: {
+        tittle,
+        content,
+      },
+      where: {
+        id,
+      },
+    });
+
     return response.json();
   }
 
   async remove(request: Request, response: Response) {
+    const {id }= request.params;
+
+    await prisma.question.delete({ where: { id } });
     return response.json();
   }
 }
